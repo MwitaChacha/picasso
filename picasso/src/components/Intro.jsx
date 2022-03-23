dbdf249becc826f0c78baa88d765fbc690f44452
Mwita Chacha <jonahhchacha@gmail.com>
2022-03-23 16:04:15 +0300

    Creating the intro section

---
+++ picasso/src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import trish from "../assets/_DSC0016.jpg"
+
+const Intro = () => {
+  return (
+    <div>
+      <div className="my-8">
+        <h3 className="text-lg tracking-wide ">
+          "<br />
+          YOU'VE ALWAYS HAD THE POWER, YOU JUST HAD TO LEARN IT FOR YOURSELF
+          <br />"
+        </h3>
+      </div>
+      <div>
+          <img className="w-60 mx-auto shadow-2xl" src={trish} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
