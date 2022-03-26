8ea93a468c46b7c6b090b710b1757aad07a022b6
Mwita Chacha <jonahhchacha@gmail.com>
2022-03-26 16:44:43 +0300

    Updating the navbar

--- picasso/src/components/Navbar.jsx
+++ picasso/src/components/Navbar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="text-left font-bold p-10 text-xs">
+    <div className="text-left font-bold p-10 text-xs flex justify-between">
       <p>chacha</p>
+      <p>welcome</p>
     </div>
   );
 }
