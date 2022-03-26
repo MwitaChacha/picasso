852852c4e4c6c9cfbf0899e1497c85bffecf4acc
Mwita Chacha <jonahhchacha@gmail.com>
2022-03-26 16:45:16 +0300

    Adding images and text in the Second component

--- picasso/src/components/Second.jsx
+++ picasso/src/components/Second.jsx
@@ -6,21 +6,29 @@ import sparkles from "../assets/sparkles.png";
 
 const Second = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 h-screen pic-section">
-      <div className=" text-sm sunflower-section h-full">
+    <div className="md:flex flex-row justify-around my-20">
+      <div className=" text-sm my-8">
         <div className="items-center">
-          <img className="w-60 m-auto shadow-2xl" src={sunflower} alt="" />
-          <p>
-            "You've made it to my thoughts, welcome. You are going to stay for a
-            while"
+          <img
+            className="w-60 h-64 m-auto shadow-2xl object-cover"
+            src={sunflower}
+            alt=""
+          />
+          <p className="px-32 my-5 text-xs font-bold text-amber-500">
+            You've made it to my thoughts, welcome. You are going to stay for a
+            while
           </p>
         </div>
       </div>
-      <div className=" text-sm sparkles-section h-full content-center">
-        <img className="w-64 mx-auto shadow-2xl" src={sparkles} alt="" />
-        <p>
-          "Your battles inspired me; not the obvious material battles, but the
-          ones that were won behind your forehead"
+      <div className=" text-sm my-8">
+        <img
+          className="w-60 h-64 m-auto shadow-2xl object-cover"
+          src={sparkles}
+          alt=""
+        />
+        <p className="px-32 my-5 text-xs font-bold text-amber-500">
+          Your battles inspired me; not the obvious material battles, but the
+          ones that were won behind your forehead
         </p>
       </div>
     </div>
