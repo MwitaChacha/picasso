98f09a387701fb7895fba32ab96af471159acb16
Mwita Chacha <jonahhchacha@gmail.com>
2022-07-17 18:37:12 +0300

    Restructuring the App

--- picasso/src/App.js
+++ picasso/src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Intro from './components/Intro';
-import Navbar from './components/Navbar';
-import Second from './components/Second';
-import Third from './components/Third';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Intro />
-      <Second />
-      <Third />
+    <p>Go forth and have no fear</p>
     </div>
   );
 }
