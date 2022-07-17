82dad6508ed844f521f3b0b147c00e2b1f076fdc
Mwita Chacha <jonahhchacha@gmail.com>
2022-07-17 21:29:27 +0300

    Creating an array of image objects

--- picasso/src/App.js
+++ picasso/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 
-
 function App() {
   return (
     <div className="App">
-    <p>Go forth and have no fear</p>
     </div>
   );
 }
---
+++ picasso/src/constants/constants.jsx
@@ -0,0 +1,119 @@
+import image1 from "../assets/images/hand.jpg";
+import image2 from "../assets/images/ashley.jpg";
+import image3 from "../assets/images/sparkles.png";
+import image4 from "../assets/images/happy.png";
+import image5 from "../assets/images/robin.jpg";
+import image6 from "../assets/images/dark.jpg";
+import image7 from "../assets/images/jump.png";
+import image8 from "../assets/images/trishsunset.png";
+import image9 from "../assets/images/jaleel.jpg";
+import image10 from "../assets/images/lolo.jpg";
+import image11 from "../assets/images/angie.jpg";
+import image12 from "../assets/images/sky.jpg";
+import image13 from "../assets/images/willis.jpg";
+import image14 from "../assets/images/rubik.png";
+import image15 from "../assets/images/van.jpg";
+import image16 from "../assets/images/eva.png";
+import image17 from "../assets/images/felix.jpg";
+
+
+
+const photos = [
+  {
+    id: 1,
+    image: image1,
+    quote:
+      "All of the stories, the hero gets lonely. Now is the time to show what you are made of.",
+  },
+  {
+    id: 2,
+    image: image2,
+    quote:
+      "Perhaps happiness is always to be found in the journey uphill, and not in the fleeting sense of satisfaction awaiting at the next peak.",
+  },
+  {
+    id: 3,
+    image: image3,
+    quote: "Go forth and have no fear.",
+  },
+  {
+    id: 4,
+    image: image4,
+    quote: "Go forth and have no fear.",
+  },
+  {
+    id: 5,
+    image: image5,
+    quote:
+      "You went out in search of gold far wide. But all along you were gold on the inside.",
+  },
+  {
+    id: 6,
+    image: image6,
+    quote:
+      "There will come a time a time when you believe that everything is finished; that will be the beginning.",
+  },
+  {
+    id: 7,
+    image: image7,
+    quote:
+      "One of these days I hope you'll know the direction you want to take. But when you know, you have to start immediately, you cannot afford to waste time, not you.",
+  },
+  {
+    id: 8,
+    image: image8,
+    quote: "That it will never come again is what makes life so sweet.",
+  },
+  {
+    id: 9,
+    image: image9,
+    quote:
+      "The greatest happiness of life is the conviction that we are loved; loved for ourselves, or rather, loved in spite of ourselves.",
+  },
+  {
+    id: 10,
+    image: image10,
+    quote:
+      "Change your life today. Don't gamble on the future, act now, without delay.",
+  },
+  {
+    id: 11,
+    image: image11,
+    quote:
+      "I have not ceased to be fearful, but I have ceased to let fear control me.",
+  },
+  {
+    id: 12,
+    image: image12,
+    quote: "The most commonform of despair is not being who you are.",
+  },
+  {
+    id: 13,
+    image: image13,
+    quote: "And you? When will you begin that long journey into yourself?.",
+  },
+  {
+    id: 14,
+    image: image14,
+    quote:
+      "To see a man beaten not by a better opponent but by himself is a tragedy.",
+  },
+  {
+    id: 15,
+    image: image15,
+    quote: "To stand straight, not straightened.",
+  },
+  {
+    id: 16,
+    image: image16,
+    quote: "While we are postponing, life speeds by.",
+  },
+  {
+    id: 17,
+    image: image17,
+    quote:
+      "If everything around seems dark, look again, you may be the light.",
+  },
+];
+
+export default photos;
